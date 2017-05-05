e55a499221fc8150ffecd1bddb19cd3fa9a60f3f
Andy Loftus <aloftus@gmail.com>
2017-05-05 13:36:20 -0500

    Auto-yes for apt-get. Save output in install.log

--- install.sh
+++ install.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
+set -x
+
+(
 WWW=/var/www/html
 PRGDIR=$(dirname $0)
 PYTHON_PKGS=( dropbox )
 
-#fn="$PRGDIR/bash.common"
-#[[ -r $fn ]] || { echo "Cant access file '$fn'"; exit 1
-#}
-#source $fn
 
 # Ensure python3
-set -x
 sudo apt-get update
-sudo apt-get install python3 python3-pip
+sudo apt-get -y install python3 python3-pip
 # Install required python packages
 sudo pip3 install "${PYTHON_PKGS[@]}"
-set +x
 
 css="$PRGDIR/dygraph.2.0.0.min.css"
 js="$PRGDIR/dygraph.2.0.0.min.js"
 for x in css js; do
     fn_src=${!x}
     fn_tgt=$WWW/$x/
-    set -x
     sudo rsync -ivptgo "$fn_src" "$fn_tgt"
-    set +x
 done
 
 # Enable user 'pi' to write to html/data area
 sudo usermod -a -G www-data pi
+) | tee "$(dirname $0)/install.log"
